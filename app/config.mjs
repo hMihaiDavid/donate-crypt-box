e1fd3a6ffae77c13d1df08b744f580f1472c14f0
Horia Mihai David <mihaidavid@posteo.net>
2025-02-14 18:11:33 +0100

    improve config

--- app/config.mjs
+++ app/config.mjs
@@ -3,18 +3,17 @@ import _ from 'lodash';
 
 export const DEFAULT_LEASE_MAX_AGE_SECS = 60 * 60 * 24 * 7 * 1;
 export const DEFAULT_LEASE_SET_SIZE = 5;
-export const DEFAULT_NUM_GIVEN_ADDRESSES = 1;
+export const DEFAULT_GIVE_FULL_LEASE_SET = false;
 
-const leaseMaxAgeSecs = process.env.LEASE_MAX_AGE_SECS || DEFAULT_LEASE_MAX_AGE_SECS;
+const leaseMaxAgeSecs = Number(process.env.LEASE_MAX_AGE_SECS) || DEFAULT_LEASE_MAX_AGE_SECS;
 assert(_.isInteger(leaseMaxAgeSecs) && leaseMaxAgeSecs >= 0, "LEASE_MAX_AGE_SECS must be a positive integer or 0.");
 
-const leaseSetSize = process.env.LEASE_SET_SIZE || DEFAULT_LEASE_SET_SIZE;
+const leaseSetSize = Number(process.env.LEASE_SET_SIZE) || DEFAULT_LEASE_SET_SIZE;
 assert(_.isInteger(leaseSetSize) && leaseSetSize > 0, "LEASE_SET_SIZE must be a positive integer.")
 
-const numGivenAddresses = process.env.NUM_GIVEN_ADDRESSES || DEFAULT_NUM_GIVEN_ADDRESSES;
-assert(_.isInteger(numGivenAddresses) && numGivenAddresses > 0, "NUM_GIVEN_ADDRESSES must be a positive integer.")
-assert(numGivenAddresses <= leaseSetSize, "NUM_GIVEN_ADDRESSES must be <= LEASE_SET_SIZE");
+let giveFullLeaseSet = process.env.GIVE_FULL_LEASE_SET;
+giveFullLeaseSet = giveFullLeaseSet !== undefined ? Boolean(giveFullLeaseSet) : DEFAULT_GIVE_FULL_LEASE_SET;
 
 export const LEASE_MAX_AGE_SECS = leaseMaxAgeSecs;
 export const LEASE_SET_SIZE = leaseSetSize;
-export const NUM_GIVEN_ADDRESSES = numGivenAddresses;
+export const GIVE_FULL_LEASE_SET = giveFullLeaseSet;
